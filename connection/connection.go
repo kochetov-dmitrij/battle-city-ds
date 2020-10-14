b78e40ea043eab5681c6a93bc340253e407a27a0
fenchelfen <m.lyamets@innopolis.ru>
2020-10-14 21:36:45 +0300

    Sketch discovery/advertise

--- connection/connection.go
+++ connection/connection.go
@@ -1,5 +1,43 @@
 package connection
 
+import (
+	"fmt"
+	"github.com/huin/goupnp"
+	"github.com/huin/goupnp/httpu"
+	"github.com/huin/goupnp/ssdp"
+	"sync"
+)
 
+var wg sync.WaitGroup
 
-// something
+func Advertise() {
+	client, _ := httpu.NewHTTPUClient()
+	r, _ := ssdp.SSDPRawSearch(client, ssdp.SSDPAll, 5, 9999)
+
+	fmt.Printf("%v\n", r)
+	wg.Done()
+}
+
+func Discover() {
+
+	fmt.Printf("Lol kakoi discover ..\n")
+
+	r, err := goupnp.DiscoverDevices(ssdp.SSDPAll)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", r)
+
+	wg.Done()
+}
+
+func Connection() {
+
+	wg.Add(2)
+
+	go Advertise()
+	go Discover()
+
+	wg.Wait()
+}
